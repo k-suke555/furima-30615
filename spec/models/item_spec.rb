7f798e92a5e977c7a4f4716a321012137ce0a493
k-suke555 <keisuke.k.1991@gmail.com>
2020-10-21 19:34:32 +0900

    商品出品機能実装の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  before do
-    @item = FactoryBot.build(:item)
-  end
-
   describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it '全ての情報が正しく入力できていれば保存できること' do
+      expect(@item).to be_valid
+    end
+
     it '商品画像を1枚つけることが必須であること' do
       @item.image = nil
       @item.valid?
@@ -30,10 +34,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
+    it 'カテゴリーについては---が選択されていると保存できないこと' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category must be other than 1')
+    end
+
     it '商品の状態についての情報が必須であること' do
       @item.status = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+
+    it '商品の状態については---が選択されていると保存できないこと' do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Status must be other than 1')
     end
 
     it '配送料の負担についての情報が必須であること' do
@@ -42,16 +58,34 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Burden can't be blank")
     end
 
+    it '配送料の負担については---が選択されていると保存できないこと' do
+      @item.burden_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Burden must be other than 1')
+    end
+
     it '発送元の地域についての情報が必須であること' do
       @item.area = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Area can't be blank")
     end
 
+    it '発送元の地域については---が選択されていると保存できないこと' do
+      @item.area_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Area must be other than 1')
+    end
+
     it '発送までの日数についての情報が必須であること' do
       @item.days_to_ship = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Days to ship can't be blank")
+    end
+
+    it '発送までの日数については---が選択されていると保存できないこと' do
+      @item.days_to_ship_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
     end
 
     it '価格についての情報が必須であること' do
