aac8ca4f46128bd86403d0590fae3e3eb37ec6c0
k-suke555 <keisuke.k.1991@gmail.com>
2020-10-27 20:44:49 +0900

    コメントアウトの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,7 +20,6 @@ class OrdersController < ApplicationController
   end
  
   private
-   # 全てのストロングパラメーターを1つに統合
   def order_address_params
    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
@@ -28,9 +27,9 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: order_address_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,
+      card: order_address_params[:token], 
+      currency: 'jpy'                
     )
   end
 
